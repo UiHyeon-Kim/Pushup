f3aa452037cbbc3bf312d4cd541164b5c754ae83
김의현 <hyunkim6790@gmail.com>
2025-12-19 20:04:54 +0900

    feat: Hilt 설정

--- presentation/src/main/java/com/hanhyo/presentation/MainActivity.kt
+++ presentation/src/main/java/com/hanhyo/presentation/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.hanhyo.presentation.ui.theme.PushupTheme
+import com.hanhyo.presentation.designsystem.theme.PushupTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ presentation/src/main/java/com/hanhyo/presentation/PushupApplication.kt
@@ -0,0 +1,9 @@
+package com.hanhyo.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PushupApplication : Application() {
+
+}
