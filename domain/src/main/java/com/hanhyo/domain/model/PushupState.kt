46c65fb0cc22d95edbd3cfaaeaf3072078874651
김의현 <hyunkim6790@gmail.com>
2025-12-20 17:35:03 +0900

    feat: 푸쉬업 도메인 모델 추가
    푸쉬업 세션과 상태를 나타내는 도메인 모델을 추가
    
    - `PushupSession`: 푸쉬업 운동 세션의 정보를 담는 데이터 클래스
    - `PushupState`: 푸쉬업 동작 상태를 나타내는 sealed class (Far, Near, Unknown)

--- domain/src/main/java/com/hanhyo/domain/model/PushupState.kt
+++ domain/src/main/java/com/hanhyo/domain/model/PushupState.kt
@@ -1,6 +1,13 @@
 package com.hanhyo.domain.model
 
-enum class PushupState {
-    UP,
-    DOWN,
+/**
+ * 푸쉬업 상태
+ * @property PushupState.Far 푸쉬업 중 멀어졌을 때
+ * @property PushupState.Near 푸쉬업 중 가까워졌을 때
+ * @property PushupState.Unknown 센서 상태를 알 수 없을 때
+ */
+sealed class PushupState {
+    object Far : PushupState()
+    object Near : PushupState()
+    object Unknown : PushupState()
 }
