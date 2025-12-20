48852318e57c7fcf1a241608bbdfc955c7f7bf5f
김의현 <hyunkim6790@gmail.com>
2025-12-20 14:18:38 +0900

    feat: Room Database에 TypeConverter 추가 및 스키마 내보내기 설정

--- data/src/main/java/com/hanhyo/data/local/db/PushupDatabase.kt
+++ data/src/main/java/com/hanhyo/data/local/db/PushupDatabase.kt
@@ -2,6 +2,7 @@ package com.hanhyo.data.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.hanhyo.data.local.dao.PushupSessionDao
 import com.hanhyo.data.local.entity.PushupSessionEntity
 
@@ -10,7 +11,9 @@ import com.hanhyo.data.local.entity.PushupSessionEntity
         PushupSessionEntity::class,
     ],
     version = 1,
+    exportSchema = true
 )
+@TypeConverters(Converters::class)
 abstract class PushupDatabase : RoomDatabase() {
     abstract fun pushupSessionDao(): PushupSessionDao
 }
