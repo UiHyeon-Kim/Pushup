9a5726386ce2cb97fb0b6833a8a715fe99df1b17
김의현 <hyunkim6790@gmail.com>
2025-12-20 15:13:47 +0900

    feat: HiltAndroidApp EmptyClassBlock 적용
    - Hilt를 위해 선언, 아직 내부 작성할 내용이 없어서 detekt 무시 적용

--- presentation/src/main/java/com/hanhyo/presentation/PushupApplication.kt
+++ presentation/src/main/java/com/hanhyo/presentation/PushupApplication.kt
@@ -4,6 +4,5 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PushupApplication : Application() {
-
-}
+@Suppress("EmptyClassBlock") // 내부 구현 사항 생기기 전까지 Detekt 무시
+class PushupApplication : Application() 
