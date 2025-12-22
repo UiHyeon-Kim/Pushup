c2a0ba03555bb4d19d74f78d5e2f27ce1bff273c
김의현 <hyunkim6790@gmail.com>
2025-12-22 22:48:08 +0900

    feat: 팔굽혀펴기 세션 ID 추가
    - `PushupUiState`에 `sessionId` 필드를 추가하여 현재 팔굽혀펴기 세션을 식별할 수 있도록 함.

--- presentation/src/main/java/com/hanhyo/presentation/ui/pushup/PushupUiState.kt
+++ presentation/src/main/java/com/hanhyo/presentation/ui/pushup/PushupUiState.kt
@@ -3,6 +3,7 @@ package com.hanhyo.presentation.ui.pushup
 import com.hanhyo.domain.model.PushupState
 
 data class PushupUiState(
+    val sessionId: Long? = null,
     val currentCount: Int = 0,
     val sessionStartTime: Long? = null,
     val sessionDuration: Int = 0,
