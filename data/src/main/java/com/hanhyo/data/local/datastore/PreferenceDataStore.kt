320991c61a99eb027822a57d473bae73018b1849
김의현 <hyunkim6790@gmail.com>
2025-12-23 02:14:44 +0900

    feat: PreferenceDataStore 추가

---
+++ data/src/main/java/com/hanhyo/data/local/datastore/PreferenceDataStore.kt
@@ -0,0 +1,41 @@
+package com.hanhyo.data.local.datastore
+
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.hanhyo.domain.model.UserPreference
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+private val Context.dataStore by preferencesDataStore(name = "preferences")
+
+class PreferenceDataStore @Inject constructor(
+    @ApplicationContext private val context: Context,
+) {
+
+    val preference: Flow<UserPreference> = context.dataStore.data
+        .catch { emit(emptyPreferences()) }
+        .map { preferences ->
+            UserPreference(
+                vibrationEnabled = preferences[VIBRATION] ?: true,
+                soundEnabled = preferences[SOUND] ?: true,
+            )
+        }
+
+    suspend fun update(preference: UserPreference) {
+        context.dataStore.edit { prefer ->
+            prefer[VIBRATION] = preference.vibrationEnabled
+            prefer[SOUND] = preference.soundEnabled
+        }
+    }
+
+    companion object Keys {
+        val VIBRATION = booleanPreferencesKey("vibration")
+        val SOUND = booleanPreferencesKey("sound")
+    }
+}
