4442784dae7c2b0dde4ab8dea33670bbd7003e16
김의현 <hyunkim6790@gmail.com>
2025-12-24 16:38:38 +0900

    feat: Timber 초기화 설정

--- presentation/src/main/java/com/hanhyo/presentation/PushupApplication.kt
+++ presentation/src/main/java/com/hanhyo/presentation/PushupApplication.kt
@@ -1,8 +1,17 @@
 package com.hanhyo.presentation
 
 import android.app.Application
+import android.content.pm.ApplicationInfo
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-@Suppress("EmptyClassBlock") // 내부 구현 사항 생기기 전까지 Detekt 무시
-class PushupApplication : Application() 
+class PushupApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val isDebuggable = (0 != (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE))
+        if (isDebuggable) Timber.plant(Timber.DebugTree())
+    }
+}
